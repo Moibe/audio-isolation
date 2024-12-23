7a9a783a6cb53fc8477d173d235de8a1205a10ea
Moibe <moi_estrello@hotmail.com>
2024-12-22 22:36:35 -0600

    ¡Pruebas terminadas exitosamente!

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+
+import os
+from scipy.io.wavfile import write
+import demucs.separate
+
 print("Hello song")
+
+
+os.makedirs("out", exist_ok=True)
+
+#os.system("python3 -m demucs.separate -n htdemucs --two-stems=vocals test.wav -o out")
+demucs.separate.main(["-n", "htdemucs", "--mp3", "--two-stems=vocals", "test.mp3", "-o", "out"])
+#return "./out/htdemucs/test/vocals.wav","./out/htdemucs/test/no_vocals.wav"
